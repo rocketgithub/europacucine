80a89b45d36c6c323054561335c1697e71d98914
jeffrepo <jeffer.leo93@gmail.com>
2022-02-17 13:46:53 -0600

    Serie + numero fel

--- models/account_move.py
+++ models/account_move.py
@@ -9,9 +9,9 @@ class AccountMove(models.Model):
     @api.onchange('payment_reference', 'ref','numero_fel')
     def _onchange_payment_reference(self):
         res = super()._onchange_payment_reference()
-        self.payment_reference = self.numero_fel if (self.journal_id.generar_fel and self.numero_fel) else ''
+        self.payment_reference = (self.serie_fel +"-"+ self.numero_fel) if (self.journal_id.generar_fel and self.numero_fel and self.serie_fel) else ''
         for line in self.line_ids.filtered(lambda line: line.account_id.user_type_id.type in ('receivable', 'payable')):
-            line.name =  self.numero_fel if (self.journal_id.generar_fel and self.numero_fel) else (self.payment_reference or self.ref)
+            line.name =  (self.serie_fel +"-"+ self.numero_fel) if (self.journal_id.generar_fel and self.numero_fel and self.serie_fel) else (self.payment_reference or self.ref)
 
     def _post(self,soft=True):
         res = super(AccountMove, self)._post(soft)
